7e48c668ce4109b6495271bbaa70d14fbdd8ca48
Aly <asecure05@gmail.com>
2022-10-18 22:03:00 +0200

    Fixed bug (trailer keeps playing after navigating up)

--- app/src/main/java/com/silverbullet/moviesapp/presentation/components/YoutubeView.kt
+++ app/src/main/java/com/silverbullet/moviesapp/presentation/components/YoutubeView.kt
@@ -1,12 +1,15 @@
 package com.silverbullet.moviesapp.presentation.components
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerSupportFragmentXKt
@@ -19,6 +22,22 @@ fun YoutubeView(
     videoKey: String
 ) {
     val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    var ytPlayer by remember {
+        mutableStateOf<YouTubePlayer?>(null)
+    }
+    DisposableEffect(key1 = lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_STOP) {
+                ytPlayer?.pause()
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            ytPlayer?.pause()
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
     AndroidView(
         factory = {
             val fm = (context as AppCompatActivity).supportFragmentManager
@@ -34,6 +53,7 @@ fun YoutubeView(
                             player: YouTubePlayer?,
                             wasRestored: Boolean
                         ) {
+                            ytPlayer = player
                             player?.setShowFullscreenButton(false)
                             if (!wasRestored) {
                                 player?.cueVideo(videoKey)
